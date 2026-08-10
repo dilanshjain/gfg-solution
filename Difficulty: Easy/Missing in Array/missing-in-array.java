eb8cfe06696a06ab7059defa20fc0011b2964294
DILANSH JAIN <dilanshsjain2005@gmail.com>
2026-08-11 01:05:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    int missingNum(int arr[]) {
+        
+        int n = 1 + arr.length;
+        int i = 0;
+        while(i<arr.length){
+            if(arr[i] == i+1 || arr[i] == n) i++;
+            else{
+                int idx = arr[i] - 1;
+                swap(arr , i , idx);
+            }
+        }
+        for(i = 0 ; i < arr.length ; i++){
+            if(arr[i] != i+1) return i+1;
+        }
+        return n;
+    }
+    
+    private void swap(int[] arr , int i , int idx){
+        int temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp;
+    }
+    
+    
+}
