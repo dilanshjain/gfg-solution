efc88a6fc866071999b7a5adc4c12d8de818f8f7
DILANSH JAIN <dilanshsjain2005@gmail.com>
2026-09-01 19:24:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -0,0 +1,25 @@
+
+
+class Solution {
+    public boolean isPalindrome(Node head) {
+        // code here
+        Node temp = head;
+        ArrayList<Integer> arr = new ArrayList<>();
+        while(temp != null){
+            arr.add(temp.data);
+            temp = temp.next;
+        }
+        
+        int i = 0;
+        int j = arr.size() -1;
+        while(i <= j){
+            int a = arr.get(i);
+            int b = arr.get(j);
+            if(a != b) return false;
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+}
