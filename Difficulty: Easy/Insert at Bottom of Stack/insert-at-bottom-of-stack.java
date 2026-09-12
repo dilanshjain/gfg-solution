2428ad478b249800a4f51c1e5682aa04eaa1263e
DILANSH JAIN <dilanshsjain2005@gmail.com>
2026-09-12 23:22:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insert at Bottom of Stack/insert-at-bottom-of-stack.java
@@ -0,0 +1,16 @@
+class Solution {
+    public Stack<Integer> insertAtBottom(Stack<Integer> st, int x) {
+        if (st.isEmpty()) {
+            st.push(x);
+            return st;
+        }
+
+        int top = st.pop();
+
+        insertAtBottom(st, x);
+
+        st.push(top);
+
+        return st;
+    }
+}
